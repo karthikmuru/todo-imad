991d2fbc3a5cd832c05916061cb8fe99bc2b11a6
Karthik M <karthik.murugesan@outlook.in>
2016-11-14 23:25:50 +0530

    mongoose

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine','ejs');
+
+
+mongoose.connect('mongodb://test:test@ds139327.mlab.com:39327/todo');
+
+//Create schema
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo',todoSchema);
+
+var item = Todo({item:'buy flowers'}).save(function(err){
+
+    console.log(err);
+});
+
 require('./app/routes.js')(app);
 
 app.listen(app.get('port'), function() {
